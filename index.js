f3ab9f36a4778d734710360cd5644d4cc9fe2375
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-16 17:50:30 +0200

    the save button has text content now

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-//document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-function increment() {
-    // console.log("The button was clicked")
-    count = count + 1
+
+
+//document.getElementById("count-el").innerText = 5
+function increment() { 
+    count  += 1
     countEl.innerText = count
     
-    // document.getElementById("count-el").innerText = count
+}
+// grab save-el paragraph en store its variable called save-el
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    
 }
