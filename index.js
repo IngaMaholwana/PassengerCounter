e840acff7231a7b3be479881890a83c4cd6631f8
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-16 18:02:06 +0200

    the save now reverts back to 0 once save been pressed

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ let count = 0
 //document.getElementById("count-el").innerText = 5
 function increment() { 
     count  += 1
-    countEl.innerText = count
+    countEl.textContent  = count
     
 }
 // grab save-el paragraph en store its variable called save-el
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
-    
+    //everytime we click save, we want to reset the count to 0
+    countEl.textContent = 0
+    count = 0
 }
