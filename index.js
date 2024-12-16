3756196470dbcbc51626f3bfe7de050bc92315da
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-16 16:55:23 +0200

    parce argurment done but increment only

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el")
 let count = 0
+function increment() {
+    // console.log("The button was clicked")
+    count = count + 1
+    countEl.innerText = count
+    
+    // document.getElementById("count-el").innerText = count
+}
